0e5dc278a6361b5b6b65460efbf0125349c2a1fa
HongVi711 <nguyenhongvi71103@gmail.com>
2026-03-06 20:02:28 +0700

    update

--- src/routes/tenant.route.ts
+++ src/routes/tenant.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllTenantsController, getTenantByIdController } from "../controllers/tenant.controller";
+import { getAllTenantsController, getTenantByIdController, updateTenantController } from "../controllers/tenant.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { requireRole } from "../middlewares/role.middleware";
 import { ROLE } from "../utils/app.constants";
@@ -9,5 +9,7 @@ const router = Router();
 router.get("/", authMiddleware, requireRole([ROLE.OWNER]), getAllTenantsController);
 
 router.get("/:id", authMiddleware, requireRole([ROLE.OWNER]), getTenantByIdController);
+
+router.put("/:id", authMiddleware, requireRole([ROLE.OWNER]), updateTenantController);
 
 export default router;
